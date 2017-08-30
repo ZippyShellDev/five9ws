11b757a945816febc16298a1c3407c814a2753c3
Tony Sarkisian <tonysark@gmail.com>
2017-08-29 22:51:02 -0700

    Update and rename five9.rb to five9ws.rb

---
+++ lib/five9ws.rb
@@ -0,0 +1,15 @@
+require "accessible_hash"
+require "five9ws/version"
+require "five9ws/base"
+require "five9ws/user_management"
+require "five9ws/statistics"
+require "five9ws/agent_stats.rb"
+require "five9ws/inbound_campaign_stats.rb"
+require "five9ws/acd_status.rb"
+require "five9ws/user_management.rb"
+require "five9ws/user.rb"
+require "five9ws/user_resources/skill_array.rb"
+require "five9ws/general_user.rb"
+
+module Five9ws
+end
